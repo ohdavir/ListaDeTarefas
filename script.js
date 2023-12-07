6338cc56ca9878971f9812242b01457500f5484f
ohdavir <davi.rosa.info@gmail.com>
2023-12-07 09:31:22 -0300

    terminado

--- script.js
+++ script.js
@@ -1,38 +1,43 @@
-let botaoadiconar = document.getElementById("adicionar");
+let adicionar = document.getElementById("adicionar");
 let ultarefa = document.getElementById("ultarefa");
 let input = document.getElementById("camporesultado")
-let botaofinalizar = document.getElementById("finalizar");
-let botaolimpar2 = document.getElementById("limpar2")
+let finalizar = document.getElementById("finalizar");
+let limpar = document.getElementById("limpar");
+let limpar2 = document.getElementById("limpar2")
 let ultarefacompleta = document.getElementById("ultarefacompleta")
 
 
-function adicionarTarefa() { // Função para adicionar uma tarefa
-    if (input.value.trim() !== "") { // Verifica se o campo de input não está vazio
-        let lista = document.createElement("li"); // Cria um parágrafo (<p>) para a nova tarefa
-        lista.innerText = input.value; // Define o texto do parágrafo com o valor do campo de input
-        ultarefa.appendChild(lista); // Adiciona o li ao container de tarefas pendentes
-        input.value = ""; // Limpa o valor do campo de input
-        input.focus(); // Define o foco de volta ao campo de input
-        lista.addEventListener("click", function () { // Adiciona um ouvinte de evento para o clique no parágrafo
-            ultarefa.removeChild(lista);// Remove o parágrafo do container de tarefas pendentes
-            ultarefacompleta.appendChild(lista);// Adiciona o parágrafo ao container de tarefas completas
+function adicionarTarefa() { // função para adicionar uma tarefa
+    if (input.value.trim() !== "") { 
+        let lista = document.createElement("li"); // cria um ul para a nova tarefa
+        lista.innerText = input.value; // texto do ul com o valor do campo de input
+        ultarefa.appendChild(lista); // adiciona o li ao container de tarefas pendentes
+        input.value = ""; // limpa o valor do campo de input
+        input.focus(); // foco de volta ao campo de input
+        lista.addEventListener("click", function () { // Adiciona um ouvinte de evento para o clique no ul
+            ultarefa.removeChild(lista);// Remove o ul de tarefas pendentes
+            ultarefacompleta.appendChild(lista);// Adiciona o ul a tarefas completas
         });
-        botaolimpar.addEventListener("click", function () { // Adiciona um ouvinte de evento para o clique no botão de limpar
-            ultarefa.removeChild(lista); // Remove o parágrafo do container de tarefas pendentes
-            ultarefacompleta.appendChild(lista); // Adiciona o parágrafo ao container de tarefas completas
-            input.focus(); // Define o foco de volta ao campo de input
+        finalizar.addEventListener("click", function () { // evento pra finalizar tudo
+            ultarefa.removeChild(lista); // remove o ul do de tarefas pendentes
+            ultarefa.appendChild(lista); // adiciona o ul ao container de tarefas completas
+            input.focus(); // foco de volta ao campo de input
         });
-        botaolimpar2.addEventListener("click", function () { // Adiciona um ouvinte de evento para o clique no segundo botão de limpar 
-            ultarefacompleta.removeChild(lista);// Remove o parágrafo do container de tarefas completas
-            input.focus(); // Define o foco de volta ao campo de input
+        limpar.addEventListener("click", function () { // evento pra apagar as lista
+            ultarefa.removeChild(lista);// remove o ul de tarefas pendentes
+            input.focus(); // foco de volta ao campo de input
+        });
+        limpar2.addEventListener("click", function () { // evento pra apagar as lista
+            ultarefacompleta.removeChild(lista);// remove o ul de tarefas completas
+            input.focus(); // foco de volta ao campo de input
         });
     }
 }
-// Adiciona um ouvinte de evento para o clique no botão de adicionar
-botaoadiconar.addEventListener("click", adicionarTarefa);
-// Adiciona um ouvinte de evento para a tecla Enter
+// evento pra finalizar tarefa
+adicionar.addEventListener("click", adicionarTarefa);
+// evento para a tecla Enter
 document.addEventListener("keypress", function (event) { // Verifica se a tecla pressionada é Enter (código 13)
-    if (event.keyCode === 13) { // Chama a função para adicionar uma tarefa
+    if (event.keyCode === 13) { // Chama a função para adiciona r uma tarefa
         adicionarTarefa();
     }
 });
