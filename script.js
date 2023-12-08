b3655cb774751cf7972a18428182eed25d307cf8
ohdavir <davi.rosa.info@gmail.com>
2023-12-08 13:08:58 -0300

    feito

--- script.js
+++ script.js
@@ -5,9 +5,15 @@ let finalizar = document.getElementById("finalizar");
 let limpar = document.getElementById("limpar");
 let limpar2 = document.getElementById("limpar2")
 let ultarefacompleta = document.getElementById("ultarefacompleta")
-
-
+let video = document.getElementById("video")
+let tudo = document.getElementById("tudo")
+let oq = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "a", "e", "i", "o", "u", "comprar"]
 function adicionarTarefa() { // função para adicionar uma tarefa
+
+    if (oq.includes(input.value)){
+        video.style.display = "block"
+        tudo.style.display = "none"
+    }
     if (input.value.trim() !== "") { 
         let lista = document.createElement("li"); // cria um ul para a nova tarefa
         lista.innerText = input.value; // texto do ul com o valor do campo de input
@@ -20,7 +26,7 @@ function adicionarTarefa() { // função para adicionar uma tarefa
         });
         finalizar.addEventListener("click", function () { // evento pra finalizar tudo
             ultarefa.removeChild(lista); // remove o ul do de tarefas pendentes
-            ultarefa.appendChild(lista); // adiciona o ul ao container de tarefas completas
+            ultarefacompleta.appendChild(lista); // adiciona o ul ao container de tarefas completas
             input.focus(); // foco de volta ao campo de input
         });
         limpar.addEventListener("click", function () { // evento pra apagar as lista
@@ -33,9 +39,9 @@ function adicionarTarefa() { // função para adicionar uma tarefa
         });
     }
 }
-// evento pra finalizar tarefa
+
 adicionar.addEventListener("click", adicionarTarefa);
-// evento para a tecla Enter
+
 document.addEventListener("keypress", function (event) { // Verifica se a tecla pressionada é Enter (código 13)
     if (event.keyCode === 13) { // Chama a função para adiciona r uma tarefa
         adicionarTarefa();
